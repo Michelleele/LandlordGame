c56c8e574616b75f9e9d0b54d06e59b769979b27
BT_2S14_28 <tuasghj@tahj.tuegv>
2025-05-28 10:47:33 -0400

    1) finished method for placing all the possible pairs into an instance variable in bot 2) started on the triplets method

--- src/Bot.java
+++ src/Bot.java
@@ -27,53 +27,87 @@ public class Bot extends Player {
 
     //should I have a card variable as the parameter or a card array
     // use card!!!
-    public static void foundInArray(ArrayList<Card[]> array, Card cards) {
-
-
+    public static boolean foundInArray(ArrayList<Card[]> array, Card card) {
+        for (Card[] subArray : array) {
+            for (Card element : subArray) {
+                if (element.getValue() == card.getValue()) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     //adds all the singles (if cardArray = [3, 3, 3], method updates singles = [3]
     public void setSingles() {
         singles = new ArrayList<Card[]>();
         Card[] single;
+
         for (Card card : hand.getCardArray()) {
             single = new Card[] {card};
-             if () {
-                 singles.add(card);
+             if (!foundInArray(singles, card)) {
+                 singles.add(single);
              }
         }
     }
 
     //adds all the pairs (if cardArray = [3, 3, 4, 4], method updates pairs = [3, 4]
     public void setPairs() {
-        pairs = new ArrayList<Card>();
-        int count;
-        for (int current = 0; current < hand.getCardArray().size(); current ++) {
-            count = 0;
-            for (int next = current + 1; next < hand.getCardArray().size(); next ++) {
-                if (hand.getCardArray().get(current).getValue() == hand.getCardArray().get(next).getValue()) {
-                    count ++;
+        pairs = new ArrayList<Card[]>();
+        Card[] onePair;
+        ArrayList<Card> cardArray = hand.getCardArray();
+        Card currentCard;
+        Card nextCard;
+
+        for (int currentIndex = 0; currentIndex < cardArray.size(); currentIndex ++) {
+            onePair = new Card[2];
+            currentCard = cardArray.get(currentIndex);
+
+            for (int nextIndex = currentIndex + 1; nextIndex < cardArray.size(); nextIndex ++) {
+                nextCard = cardArray.get(nextIndex);
+                if (currentCard.getValue() == nextCard.getValue()) {
+                    onePair[0] = currentCard;
+                    onePair[1] = nextCard;
+                    nextIndex = cardArray.size();
+                    currentIndex ++;
                 }
             }
-            if ((count >= 2) && (!contains(pairs, hand.getCardArray().get(current)))) {
-                pairs.add(hand.getCardArray().get(current));
+
+            try {
+                if (!foundInArray(pairs, onePair[0])) {
+                    pairs.add(onePair);
+                }
             }
+            catch (NullPointerException e) {}
         }
     }
 
     public void setTriplets() {
-        triplets = new ArrayList<Card>();
-        int count;
-        for (int current = 0; current < hand.getCardArray().size(); current ++) {
-            count = 0;
-            for (int next = current + 1; next < hand.getCardArray().size(); next++) {
-                if (hand.getCardArray().get(current).getValue() == hand.getCardArray().get(next).getValue()) {
-                    count++;
+        triplets = new ArrayList<Card[]>();
+        Card[] oneTriplet;
+        ArrayList<Card> cardArray = hand.getCardArray();
+        Card currentCard;
+        Card nextCard;
+
+        for (int currentIndex = 0; currentIndex < cardArray.size(); currentIndex ++) {
+            oneTriplet = new Card[3];
+            currentCard = cardArray.get(currentIndex);
+
+            for (int nextIndex = currentIndex + 1; nextIndex < cardArray.size(); nextIndex ++) {
+                nextCard = cardArray.get(nextIndex);
+                if (currentCard.getValue() == nextCard.getValue()) {
+                    onePair[0] = currentCard;
+                    onePair[1] = nextCard;
+                    nextIndex = cardArray.size();
                 }
             }
-            if ((count >= 3) && (!contains(triplets, hand.getCardArray().get(current)))) {
-                triplets.add(hand.getCardArray().get(current));
+
+            try {
+                if (!foundInArray(pairs, onePair[0])) {
+                    pairs.add(onePair);
+                }
             }
+            catch (NullPointerException e) {}
         }
     }
 
